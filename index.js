2423152165621774ed5a35961e79ca5900fa57a5
Dusuan <XxDusuanxX@hotmail.com>
2024-07-31 11:58:04 -0700

    Trying to redeploy on vercel

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import express from "express";
   app.use(express.json());
 
   const port = process.env.PORT || 5000;
-
+// vercel redeploy
   app.listen(
     port,
     () => console.log("Server running")
