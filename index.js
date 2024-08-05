10a6631a6a506f1e2182154a51b964546d4a40cd
Dusuan <XxDusuanxX@hotmail.com>
2024-08-05 08:57:24 -0700

    trying

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import express from "express";
   app.use(express.json());
 
   const port = process.env.PORT || 5000;
-// vercel redeploy
+
   app.listen(
     port,
     () => console.log("Server running")
